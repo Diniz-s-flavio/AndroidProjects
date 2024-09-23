2b3459ae7bfe6141b15213791f7ab95fcabe7434
Flávio Diniz de Sousa <102635915+Diniz-s-flavio@users.noreply.github.com>
2024-09-23 08:01:28 -0300

    23/09/24 - Login

---
+++ Login/app/src/main/java/com/ifgoiano/urt/login/HomeActivity.kt
@@ -0,0 +1,29 @@
+package com.ifgoiano.urt.login
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val welcomeTextView = findViewById<TextView>(R.id.welcomeTextView)
+
+        // Receber o nome do usuário passado como parâmetro
+        val username = intent.getStringExtra("username")
+
+        // Exibir a mensagem de boas-vindas
+        welcomeTextView.text = "Bem-vindo, $username!"
+    }
+}
---
+++ Login/app/src/main/java/com/ifgoiano/urt/login/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.ifgoiano.urt.login
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    lateinit var usernameTextField: EditText
+    lateinit var passwordTextField: EditText
+    lateinit var submitLoginButton: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        usernameTextField =  findViewById(R.id.usernameTextField)
+        passwordTextField = findViewById(R.id.passwordTextField)
+        submitLoginButton = findViewById(R.id.submitLoginButton)
+
+        submitLoginButton.setOnClickListener {
+            val username = usernameTextField.text.toString()
+            val password = passwordTextField.text.toString()
+
+            if (username == "admin" && password == "1234") {
+                // Login correto, ir para a segunda atividade
+                val intent = Intent(this, HomeActivity::class.java)
+                intent.putExtra("username", username)
+                startActivity(intent)
+            } else {
+                // Informar ao usuário que as credenciais estão incorretas
+                Toast.makeText(this, "Usuário ou senha incorretos", Toast.LENGTH_SHORT).show()
+
+                // Limpar os campos de texto
+                usernameTextField.text.clear()
+                passwordTextField.text.clear()
+            }
+        }
+    }
+}
