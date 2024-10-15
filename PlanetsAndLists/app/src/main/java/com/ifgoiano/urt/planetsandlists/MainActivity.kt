0150cc2cf32e2f018390476fa1fbec36b29aac49
Flávio Diniz de Sousa <102635915+Diniz-s-flavio@users.noreply.github.com>
2024-10-15 09:51:12 -0300

    15/10/24 - Estados Inicio

---
+++ PlanetsAndLists/app/src/main/java/com/ifgoiano/urt/planetsandlists/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.ifgoiano.urt.planetsandlists
+
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.Spinner
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    // Inicializa as views apenas quando necessário
+    private val imagem: ImageView by lazy { findViewById(R.id.img) }
+    private val combo: Spinner by lazy { findViewById(R.id.comboPlanetas) }
+    val planetas = Planetas()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        combo.adapter = ArrayAdapter(
+            this, android.R.layout.simple_spinner_item, planetas.getPlaneta()
+        ).apply {
+            setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        }
+        // Se selecionar algum planeta, atualiza a imagem
+        combo.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, posicao: Int, id: Long) {
+                // Atualiza a imagem
+                planetas.getImagemPlaneta(posicao)?.let { imagem.setImageResource(it) }
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) { /*TODO("Not yet implemented“ )*/ }
+            }
+        }
+    }
