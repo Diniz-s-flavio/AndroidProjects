1011b68d1e652759ae6217f9c72d925591f8d2ef
Flávio Diniz de Sousa <102635915+Diniz-s-flavio@users.noreply.github.com>
2024-10-18 10:36:57 -0300

    18/10/24 -  v2 -  RecyclerView done

--- EstadosRecyclerView/app/src/main/java/com/ifgoiano/urt/estadosrecyclerview/EstadoActivity.kt
+++ EstadosRecyclerView/app/src/main/java/com/ifgoiano/urt/estadosrecyclerview/EstadoActivity.kt
@@ -1,15 +1,26 @@
 package com.ifgoiano.urt.estadosrecyclerview
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class EstadoActivity : AppCompatActivity() {
+    lateinit var tvEstadoName: TextView
+    lateinit var tvEstadoCapital: TextView
+    lateinit var tvEstadoPopulacao: TextView
+    lateinit var tvEstadoRegiao: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_estado)
+        
+        tvEstadoName = findViewById(R.id.tvEstadoName)
+        tvEstadoCapital = findViewById(R.id.tvEstadoCapital)
+        tvEstadoPopulacao = findViewById(R.id.tvEstadoPopulacao)
+        tvEstadoRegiao = findViewById(R.id.tvEstadoRegiao)
     }
 }
