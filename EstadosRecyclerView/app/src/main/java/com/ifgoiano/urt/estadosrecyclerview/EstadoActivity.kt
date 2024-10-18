2301917a039f60462123300d3d4f61fe9dcd8084
Flávio Diniz de Sousa <102635915+Diniz-s-flavio@users.noreply.github.com>
2024-10-18 10:41:34 -0300

    18/10/24 - v3 - Update EstadosRecycler

--- EstadosRecyclerView/app/src/main/java/com/ifgoiano/urt/estadosrecyclerview/EstadoActivity.kt
+++ EstadosRecyclerView/app/src/main/java/com/ifgoiano/urt/estadosrecyclerview/EstadoActivity.kt
@@ -1,6 +1,7 @@
 package com.ifgoiano.urt.estadosrecyclerview
 
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -8,19 +9,33 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class EstadoActivity : AppCompatActivity() {
-    lateinit var tvEstadoName: TextView
-    lateinit var tvEstadoCapital: TextView
-    lateinit var tvEstadoPopulacao: TextView
-    lateinit var tvEstadoRegiao: TextView
+    private lateinit var img: ImageView
+    private lateinit var tvEstadoName: TextView
+    private lateinit var tvEstadoCapital: TextView
+    private lateinit var tvEstadoPopulacao: TextView
+    private lateinit var tvEstadoRegiao: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_estado)
-        
+
+        img = findViewById(R.id.img)
         tvEstadoName = findViewById(R.id.tvEstadoName)
         tvEstadoCapital = findViewById(R.id.tvEstadoCapital)
         tvEstadoPopulacao = findViewById(R.id.tvEstadoPopulacao)
         tvEstadoRegiao = findViewById(R.id.tvEstadoRegiao)
+
+        val estadoImg = intent.getIntExtra("estadoImg", 0)
+        val estadoNome = intent.getStringExtra("estadoNome")
+        val estadoCapital = intent.getStringExtra("estadoCapital")
+        val estadoPopulacao = intent.getStringExtra("estadoPopulacao")
+        val estadoRegiao = intent.getStringExtra("estadoRegiao")
+
+        img.setImageResource(estadoImg)
+        tvEstadoName.text = "Estado: " +estadoNome
+        tvEstadoCapital.text = "Capital: " +estadoCapital
+        tvEstadoPopulacao.text = "População: " + estadoPopulacao
+        tvEstadoRegiao.text = "Região: " + estadoRegiao
     }
 }
