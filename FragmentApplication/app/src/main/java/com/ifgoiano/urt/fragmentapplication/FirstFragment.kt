289e88e18735c176518e41a37e78b7d0ceb970bf
Flávio Diniz de Sousa <102635915+Diniz-s-flavio@users.noreply.github.com>
2024-11-05 08:23:21 -0300

    05/11/24 - Fragment

--- FragmentApplication/app/src/main/java/com/ifgoiano/urt/fragmentapplication/FirstFragment.kt
+++ FragmentApplication/app/src/main/java/com/ifgoiano/urt/fragmentapplication/FirstFragment.kt
@@ -8,14 +8,8 @@ import androidx.fragment.app.Fragment
 
 class FirstFragment : Fragment() {
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false)
-    }
-}
-                }
-            }
     }
 }
--- FragmentApplication/app/src/main/java/com/ifgoiano/urt/fragmentapplication/MainActivity.kt
+++ FragmentApplication/app/src/main/java/com/ifgoiano/urt/fragmentapplication/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.ifgoiano.urt.fragmentapplication
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.ifgoiano.urt.fragmentapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        replaceFragmente(FirstFragment())
+
+        binding.buttonFragment1.setOnClickListener{
+            replaceFragmente(FirstFragment())
         }
+
+        binding.buttonFragment2.setOnClickListener {
+            replaceFragmente(SecondFragment())
+        }
+    }
+
+    private fun replaceFragmente(fragment: Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
     }
 }
